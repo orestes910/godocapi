746461bb7efbfdbfb54f1b5cd145456715e6dce8
Tim Brammer <tim@tim.dev.sagedining.com>
2018-06-22 17:46:00 -0400

    Altered to use queries over paths. Added subdirs.

--- main.go
+++ main.go
@@ -13,20 +13,48 @@ func main() {
 	Serve := gin.Default()
 	//gin.SetMode(gin.ReleaseMode)
 
-	Serve.GET("/:package", func(c *gin.Context) {
-		pkg := c.Param("package")
-		valid, _ := regexp.MatchString("^[a-zA-Z]+$", pkg)
-		if valid {
-			pkgCmd := exec.Command("/usr/local/go/bin/godoc", pkg)
-			pkgOut, pkgErr := pkgCmd.CombinedOutput()
-			if pkgErr != nil {
-				log.Fatal(pkgErr)
+	Serve.GET("/doc", func(c *gin.Context) {
+		pkg := c.Query("package")
+		sub := c.Query("subdir")
+		fn := c.Query("function")
+
+		queries := c.Request.URL.Query()
+		valid := true
+		for _, v := range queries {
+			if regexpAZ(v[0]) != true {
+				valid = false
+				break
 			}
-			c.String(http.StatusOK, "%s", pkgOut)
+		}
+
+		// Figure out how to successfully run godoc with missing arguments without have a condition for ever possible combination.
+		if valid == false {
+			c.String(http.StatusBadRequest, "%s", "Queries may only contain letters\n")
 		} else {
-			c.String(http.StatusBadRequest, "%s", "Package may only contain letters\n")
+			if sub != "" {
+				cmd := exec.Command("/usr/local/go/bin/godoc", pkg+"/"+sub, fn)
+				cmdOut, cmdErr := cmd.CombinedOutput()
+				if cmdErr != nil {
+					log.Fatal(cmdErr)
+				}
+				c.String(http.StatusOK, "%s", cmdOut)
+			} else {
+				cmd := exec.Command("/usr/local/go/bin/godoc", pkg, fn)
+				cmdOut, cmdErr := cmd.CombinedOutput()
+				if cmdErr != nil {
+					log.Fatal(cmdErr)
+				}
+				c.String(http.StatusOK, "%s", cmdOut)
+			}
 		}
 	})
 
 	Serve.Run(":8080")
+}
+
+func regexpAZ(str string) bool {
+	if valid, _ := regexp.MatchString("^[a-zA-Z]+$", str); !valid {
+		return false
+	}
+	return true
 }
