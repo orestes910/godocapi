d63d0fd0bfc77f241e88f0c7fb117e40b74f245b
Tim Brammer <tim@tim.dev.sagedining.com>
2018-06-22 12:25:50 -0400

    Validate package arg to prevent other shell commands

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,12 +15,17 @@ func main() {
 
 	Serve.GET("/:package", func(c *gin.Context) {
 		pkg := c.Param("package")
-		pkgCmd := exec.Command("/usr/local/go/bin/godoc", pkg)
-		pkgOut, pkgErr := pkgCmd.CombinedOutput()
-		if pkgErr != nil {
-			log.Fatal(pkgErr)
+		valid, _ := regexp.MatchString("^[a-zA-Z]+$", pkg)
+		if valid {
+			pkgCmd := exec.Command("/usr/local/go/bin/godoc", pkg)
+			pkgOut, pkgErr := pkgCmd.CombinedOutput()
+			if pkgErr != nil {
+				log.Fatal(pkgErr)
+			}
+			c.String(http.StatusOK, "%s", pkgOut)
+		} else {
+			c.String(http.StatusBadRequest, "%s", "Package may only contain letters\n")
 		}
-		c.String(http.StatusOK, "%s", pkgOut)
 	})
 
 	Serve.Run(":8080")
